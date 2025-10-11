93dee29215411e4da14b7a817049d6c0c1c44dfa
Михаил Тучков <m2030110@gmail.com>
2025-10-12 01:39:05 +0300

    save

--- PIO/src/main.cpp
+++ PIO/src/main.cpp
@@ -1,22 +1,51 @@
+
+
+
+#include <cstdint>
+#include <cstring>
 extern "C" {
     #include "main.h"
     #include "stm32f4xx_hal.h"
+    #include "stm32f4xx_hal_gpio.h"
 }
 
+
 extern "C" {
+
+    
     ADC_HandleTypeDef hadc1;
 
     UART_HandleTypeDef huart1;
     UART_HandleTypeDef huart2;
     UART_HandleTypeDef huart6;
-    
-
     void SystemClock_Config(void);
     void MX_GPIO_Init(void);
     void MX_ADC1_Init(void);
     void MX_USART6_UART_Init(void);
     void MX_USART1_UART_Init(void);
     void MX_USART2_UART_Init(void);
+    
+}
+
+
+uint8_t rx_byte;
+int flag = 0;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart != &huart2){
+                        HAL_UART_Receive_IT(&huart2, (uint8_t*)&rx_byte, 1);
+        return;
+    }
+        if (rx_byte == 'q') {
+            flag = 1;
+        } else {
+            flag = 2;
+        }
+
+        // Перезапускаем приём следующего байта
+                HAL_UART_Receive_IT(&huart2, (uint8_t*)&rx_byte, 1);
+
+    
 }
 int main() {
     HAL_Init();
@@ -29,16 +58,27 @@ int main() {
     MX_USART2_UART_Init();
 
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-    uint8_t rx_byte;
-    HAL_StatusTypeDef status;
-    char hi[] = "bye";
-    for (;;) {
-        status = HAL_UART_Receive(&huart2, &rx_byte, 1, 100);
-        if(status == HAL_OK) {
 
-            HAL_UART_Transmit(&huart2, (uint8_t*)hi, 3,100);
+    HAL_StatusTypeDef status;
+    char hi[] = "bye\r\n";
+    uint32_t currTime = HAL_GetTick();
+    status = HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
+
+    for (;;) {
+        if (HAL_GetTick() - currTime >= 300){
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-            break;
+            currTime = HAL_GetTick();
         }
+        if (flag){
+            if (flag == 1){
+                HAL_UART_Transmit_IT(&huart2, (uint8_t*)"bye\r\n", 5);
+
+            } else if (flag==2) {
+                HAL_UART_Transmit(&huart2, (uint8_t*)"Incorrect!\r\n", 12, 100);
+
+            }
+            flag = 0;
+        }
+        
     }
 }
